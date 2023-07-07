7601c06e8b6a008502dafa451805e5ed10797729
Venkatesh Thimmaiah <venkatesh131780@gmail.com>
2023-07-07 08:05:29 +0530

    added keys to list of component

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -5,11 +5,11 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
-  const expenses = props.expenses;
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterChageHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const expenses = props.expenses;
 
   return (
     <Card className="expenses">
@@ -17,13 +17,12 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChageHandler}
       />
-      {expenses.map((expense) => (
+      {expenses.map((item) => (
         <ExpenseItem
-          title={expense.title}
-          date={expense.date}
-          amount={expense.amount}
-          locationOfExpenditure={expenses.locationOfExpenditure}
-          key={expense.id}
+          title={item.title}
+          date={item.date}
+          amount={item.amount}
+          key={item.id}
         />
       ))}
     </Card>
